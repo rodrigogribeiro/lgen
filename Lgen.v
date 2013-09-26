4185ee7686f0b7435e07f461a1434955be74f8ae
Rodrigo Ribeiro <rodrigo@mjolnir.valhalla>
2013-09-26 09:42:28 -0300

    Changes in definition of lgen_aux function.

--- Lgen.v
+++ Lgen.v
@@ -20,21 +20,33 @@ Inductive leq_ty : ty -> ty -> Prop :=
 
 Hint Constructors leq_ty.
 
+Remark leq_ty_refl : forall t, leq_ty t t.
+Proof.
+  induction t ; auto.
+Qed.
+
+Remark leq_ty_trans : forall t t', leq_ty t t' -> forall t'', leq_ty t' t'' -> leq_ty t t''.
+Proof.
+  induction 1 ; intros ; auto ;
+     match goal with
+       | [H : leq_ty (app _ _) _ |- _] => inverts* H
+       | [|- _] => idtac
+     end.
+Qed.
+
+Hint Resolve leq_ty_refl leq_ty_trans.
+
 Fixpoint fv (t : ty) : list nat :=
   match t with
     | var n => n :: nil
-    | con n    => nil
-    | app l r  => fv l ++ fv r 
+    | con n => nil
+    | app l r => fv l ++ fv r
   end.
 
-Notation "'[' x ']'" := (exist _ x _).
-Notation "'[[' x ']]'" := (inleft _ [x]).
-Notation "!!" := (inright _ _).
-
-Obligation Tactic := program_simpl ; intuition ; 
+Obligation Tactic := program_simpl ; intuition ;
               (repeat (match goal with
                        | [H : _ \/ _ |- _] => destruct H ; subst
-                       | [H : context[In _ _ -> _], 
+                       | [H : context[In _ _ -> _],
                           H1 : In _ _ |- _] => apply H in H1 ; try omega
                        end)).
 
@@ -44,7 +56,7 @@ Program Fixpoint max_list (l : list nat) : {n | forall n', In n' l -> n > n'} :=
     | x :: l' =>
         match max_list l' with
           | x' => match le_gt_dec x x' with
-                    | left _  => S x'
+                    | left _ => S x'
                     | right _ => S x
                   end
         end
@@ -52,35 +64,37 @@ Program Fixpoint max_list (l : list nat) : {n | forall n', In n' l -> n > n'} :=
 
 Definition gen_list := list (ty * ty * nat)%type.
 
-Obligation Tactic := program_simpl ; intuition ;
-     repeat 
+Obligation Tactic :=  program_simpl ; intuition ;
+     repeat
        (match goal with
-          | [H : ex _ |- _] => let v := fresh "v" in destruct H as [v H]
-          | [H : False |- _] => elim H
+          | [H : ex _ |- _] => 
+              let v := fresh "v" in destruct H as [v H]
+          | [H : False |- _] => destruct H
           | [H : _ \/ _ |- _] => destruct H
           | [H : _ = _ |- _] => inverts* H
-        end).
+        end). 
 
-Program Fixpoint lookup_gen_list (t t' : ty) (l : gen_list) : 
+
+Program Fixpoint lookup_gen_list (t t' : ty) (l : gen_list) :
   {n | In (t,t',n) l} + {~ exists n, In (t,t',n) l} :=
   match l with
     | nil => inright _ _
-    | x :: l' => 
+    | x :: l' =>
         match x with
-          | (a,b,c) => 
+          | (a,b,c) =>
               match eq_ty_dec t a, eq_ty_dec b t' with
                 | left _, left _ => inleft _ (exist _ c _)
-                | right _, left _ => 
-                    match lookup_gen_list t t' l' with
-                      | inleft x  => inleft _ x
-                      | inright _ => inright _ _
-                    end
-                | left _, right _ => 
+                | right _, left _ =>
                     match lookup_gen_list t t' l' with
                       | inleft x => inleft _ x
                       | inright _ => inright _ _
                     end
-                | right _, right _ => 
+                | left _, right _ =>
+                    match lookup_gen_list t t' l' with
+                      | inleft x => inleft _ x
+                      | inright _ => inright _ _
+                    end
+                | right _, right _ =>
                     match lookup_gen_list t t' l' with
                       | inleft x => inleft _ x
                       | inright _ => inright _ _
@@ -88,86 +102,83 @@ Program Fixpoint lookup_gen_list (t t' : ty) (l : gen_list) :
               end
         end
   end.
-
+  
 Definition least_gen (t t' t1 : ty) :=
   leq_ty t1 t /\ leq_ty t1 t' /\ forall u, leq_ty u t /\ leq_ty u t' -> leq_ty u t1.
 
-Obligation Tactic := 
-           unfold least_gen in * ; program_simpl ; intuition ; 
-               repeat (match goal with 
-                        | [H : leq_ty _ _ |- _] => inverts* H
-                        | [H : ?n = ?n -> False |- _] => elim H ; auto
-                       end).
+Remark least_gen_refl : forall t, least_gen t t t.
+Proof.
+  induction t ; unfold least_gen in * ; intuition ; auto.
+Qed.
 
-Program Fixpoint lgen_aux (t t' : ty) (l : gen_list) (m : nat) : {p | least_gen t t' (@fst ty (gen_list * nat) p)} :=
-  match t,t' with
-    | var n, var n' =>
-        match eq_nat_dec n n' with
-          | left _  => exist _ (var n, (l, m)) _
-          | right _ => 
-              match lookup_gen_list (var n) (var n') l with 
-                | inleft (exist x _) => exist _ (var x, (l,m)) _
-                | inright _ => exist _ (var m, ((var n, var n',m) :: l, S m)) _
+Hint Resolve least_gen_refl.
+
+Definition lgen_aux : forall (t t' : ty) (l : gen_list) (m : nat), {p | least_gen t t' (@fst ty (gen_list * nat) p)}.
+  refine (fix lgen_aux (t t' : ty) (l : gen_list) (m : nat) : {p | least_gen t t' (@fst ty (gen_list * nat) p)} :=
+      match t,t' with
+        | con n, con n' =>
+          match eq_nat_dec n n' with
+            | left _ => exist _ (con n, (l, m)) _
+            | right _ =>
+                match lookup_gen_list t t' l with
+                  | inleft (exist x _) => exist _ (var x, (l,m)) _
+                  | inright _ => exist _ (var m, ((con n, con n',m) :: l, S m)) _
+                end
+          end
+        | app f r, app f' r' =>
+          match lgen_aux f f' l m with
+            | exist v _ => 
+              match lgen_aux r r' (fst (snd v)) (snd (snd v)) with
+                | exist v' _ => exist _ (app (fst v) (fst v'), (fst (snd v') , snd (snd v'))) _
               end
-        end
-    | var n, con n' =>
-        match lookup_gen_list (var n) (con n') l with
-          | inleft (exist x _) => exist _ (var x, (l,m)) _
-          | inright _ => exist _ (var m, ((var n, con n',m) :: l, S m)) _
-        end
-    | var n, app f r =>
-        match lookup_gen_list (var n) (app f r) l with
-          | inleft (exist x _) => exist _ (var x, (l,m)) _
-          | inright _ => exist _ (var m, ((var n,app f r,m) :: l, S m)) _            
-        end
-    | con n, con n' => 
-        match eq_nat_dec n n' with
-          | left _ => exist _ (con n, (l,m)) _
-          | right _ => 
-              match lookup_gen_list (con n) (con n') l with
-                | inleft (exist x _) => exist _ (var x, (l,m)) _
-                | inright _ => exist _ (var m, ((con n,con n',m) :: l, S m)) _
-              end
-        end
-    | con n, var n' =>
-        match lookup_gen_list (con n) (var n') l with
-          | inleft (exist x _) => exist _ (var x, (l,m)) _
-          | inright _ => exist _ (var m, ((con n, var n',m) :: l, S m)) _
-        end  
-    | con n, app f r =>
-        match lookup_gen_list (con n) (app f r) l with
-          | inleft (exist x _) => exist _ (var x, (l,m)) _
-          | inright _ => exist _ (var m, ((con n, app f r,m) :: l, S m)) _
-        end    
-    | app f r, con n =>
-        match lookup_gen_list (app f r) (con n) l with
-          | inleft (exist x _) => exist _ (var x, (l,m)) _
-          | inright _ => exist _ (var m, ((app f r, con n,m) :: l, S m)) _
-        end  
-    | app f r, var n =>
-        match lookup_gen_list (app f r) (var n) l with
-          | inleft (exist x _) => exist _ (var x, (l,m)) _
-          | inright _ => exist _ (var m, ((app f r, var n,m) :: l, S m)) _
-        end    
-    | app f r, app f' r' =>
-        match lgen_aux f f' l m with
-          | (t,(l',m')) => 
-              match lgen_aux r r' l' m' with
-                | (t', (l'', m'')) => 
-                    exist _ (app t t', (l'', m'')) _
-              end
-        end 
-  end.
+          end
+        | a, b => 
+            match lookup_gen_list a b l with
+               | inleft (exist x _) => exist _ (var x, (l,m)) _
+               | inright _ => exist _ (var m, ((a, b, m) :: l, S m)) _
+            end
+      end)
+   ; clear lgen_aux ; unfold least_gen in * ; simpl in * ; intuition ; substs ; eauto ;
+     try repeat 
+         (match goal with
+             | [H : leq_ty ?u (con ?n), H0 : leq_ty ?u (con ?n') |- _] => inverts* H ; inverts* H0 ; try contradiction
+             | [H : leq_ty ?u (con _), H0 : leq_ty ?u (app _ _) |- _] => inverts* H ; inverts* H0
+             | [H : (?n = ?n) -> False |- _] => elim H ; auto
+             | [v : (ty * (gen_list * nat))%type |- _] => let va := fresh "va" in 
+                                                          let vb := fresh "vb" in
+                                                          destruct v as [v [va vb]] ; simpl in *
+             | [H : leq_ty ?u (app _ _), H' : leq_ty ?u (app _ _) |- _ ] =>
+                   inverts* H6 ; inverts* H7 ; eauto
+          end). 
+Defined.
 
 Definition lgen (t t' : ty) : {t1 | least_gen t t' t1}.
    destruct (max_list (fv t ++ fv t')) as [v H].
-   destruct (lgen_aux t t' nil v).
-   destruct x. simpl in *. exists* t0.
+   destruct (lgen_aux t t' nil v) as [[t1 [phi m]] Ht1].
+   simpl in *. exists* t1.
 Defined.
+
+Fixpoint least_gen_list (t : ty) (l : list ty) : Prop :=
+  match l with
+    | nil => True
+    | x :: l' => exists t', least_gen_list t' l' /\ least_gen x t' t
+  end.
+
+Definition lcg : forall (l : list ty), {t | least_gen_list t l} + {l = []}.   
+   refine (fix lcg (l : list ty) : {t | least_gen_list t l} + {l = []} :=
+              match l return {t | least_gen_list t l} + {l = []} with
+                | nil => inright _ _
+                | x :: l' =>
+                    match lcg l' with
+                      | inright _ => inleft _ (exist _ x _)
+                      | inleft (exist y _)  =>
+                          match lgen x y with
+                            exist z _ => inleft _ (exist _ z _)  
+                          end
+                    end
+              end
+          ) ; clear lcg ; repeat (unfold least_gen in * ; substs ; simpl in * ; intuition) ; 
+              try eexists ; splits ; eauto ; tauto.
+Defined.         
    
-Eval compute in lgen (app (app (con 0) (var 1)) (con 1)) (app (app (con 0) (con 2)) (var 1)).
 
-Definition tint := con 1.
-Definition tfloat := con 2.
-
-Eval compute in lgen (app (app (con 0) tint) tint) (app (app (con 0) tfloat) tfloat).
--- dist/Lgen.hs
+++ dist/Lgen.hs
@@ -110,84 +110,73 @@ lgen_aux :: Coq_ty -> Coq_ty -> Coq_gen_list -> Datatypes.Coq_nat ->
 lgen_aux t t' l m =
   case t of {
    Coq_var n ->
-    case t' of {
-     Coq_var n' ->
-      let {filtered_var = Peano_dec.eq_nat_dec n n'} in
-      case filtered_var of {
-       Prelude.True -> Datatypes.Coq_pair (Coq_var n) (Datatypes.Coq_pair l
-        m);
-       Prelude.False ->
-        let {filtered_var0 = lookup_gen_list (Coq_var n) (Coq_var n') l} in
-        case filtered_var0 of {
-         Prelude.Just s -> Datatypes.Coq_pair (Coq_var s) (Datatypes.Coq_pair
-          l m);
-         Prelude.Nothing -> Datatypes.Coq_pair (Coq_var m)
-          (Datatypes.Coq_pair (Datatypes.Coq_cons (Datatypes.Coq_pair
-          (Datatypes.Coq_pair (Coq_var n) (Coq_var n')) m) l) (Datatypes.S
-          m))}};
-     x ->
-      let {filtered_var = lookup_gen_list (Coq_var n) x l} in
-      case filtered_var of {
-       Prelude.Just s -> Datatypes.Coq_pair (Coq_var s) (Datatypes.Coq_pair l
-        m);
-       Prelude.Nothing -> Datatypes.Coq_pair (Coq_var m) (Datatypes.Coq_pair
-        (Datatypes.Coq_cons (Datatypes.Coq_pair (Datatypes.Coq_pair (Coq_var
-        n) x) m) l) (Datatypes.S m))}};
+    case lookup_gen_list t t' l of {
+     Prelude.Just s -> Datatypes.Coq_pair (Coq_var s) (Datatypes.Coq_pair l
+      m);
+     Prelude.Nothing -> Datatypes.Coq_pair (Coq_var m) (Datatypes.Coq_pair
+      (Datatypes.Coq_cons (Datatypes.Coq_pair (Datatypes.Coq_pair t t') m) l)
+      (Datatypes.S m))};
    Coq_con n ->
     case t' of {
      Coq_con n' ->
-      let {filtered_var = Peano_dec.eq_nat_dec n n'} in
-      case filtered_var of {
+      case Peano_dec.eq_nat_dec n n' of {
        Prelude.True -> Datatypes.Coq_pair (Coq_con n) (Datatypes.Coq_pair l
         m);
        Prelude.False ->
-        let {filtered_var0 = lookup_gen_list (Coq_con n) (Coq_con n') l} in
-        case filtered_var0 of {
+        case lookup_gen_list t t' l of {
          Prelude.Just s -> Datatypes.Coq_pair (Coq_var s) (Datatypes.Coq_pair
           l m);
          Prelude.Nothing -> Datatypes.Coq_pair (Coq_var m)
           (Datatypes.Coq_pair (Datatypes.Coq_cons (Datatypes.Coq_pair
           (Datatypes.Coq_pair (Coq_con n) (Coq_con n')) m) l) (Datatypes.S
           m))}};
-     x ->
-      let {filtered_var = lookup_gen_list (Coq_con n) x l} in
-      case filtered_var of {
+     _ ->
+      case lookup_gen_list t t' l of {
        Prelude.Just s -> Datatypes.Coq_pair (Coq_var s) (Datatypes.Coq_pair l
         m);
        Prelude.Nothing -> Datatypes.Coq_pair (Coq_var m) (Datatypes.Coq_pair
-        (Datatypes.Coq_cons (Datatypes.Coq_pair (Datatypes.Coq_pair (Coq_con
-        n) x) m) l) (Datatypes.S m))}};
+        (Datatypes.Coq_cons (Datatypes.Coq_pair (Datatypes.Coq_pair t t') m)
+        l) (Datatypes.S m))}};
    Coq_app f r ->
     case t' of {
      Coq_app f' r' ->
-      let {filtered_var = Specif.proj1_sig (lgen_aux f f' l m)} in
-      case filtered_var of {
-       Datatypes.Coq_pair t0 p ->
-        case p of {
-         Datatypes.Coq_pair l' m' ->
-          let {filtered_var0 = Specif.proj1_sig (lgen_aux r r' l' m')} in
-          case filtered_var0 of {
-           Datatypes.Coq_pair t'0 p0 ->
-            case p0 of {
-             Datatypes.Coq_pair l'' m'' -> Datatypes.Coq_pair (Coq_app t0
-              t'0) (Datatypes.Coq_pair l'' m'')}}}};
-     x ->
-      let {filtered_var = lookup_gen_list (Coq_app f r) x l} in
-      case filtered_var of {
+      let {v = lgen_aux f f' l m} in
+      let {
+       v' = lgen_aux r r' (Datatypes.fst (Datatypes.snd v))
+              (Datatypes.snd (Datatypes.snd v))}
+      in
+      Datatypes.Coq_pair (Coq_app (Datatypes.fst v) (Datatypes.fst v'))
+      (Datatypes.Coq_pair (Datatypes.fst (Datatypes.snd v'))
+      (Datatypes.snd (Datatypes.snd v')));
+     _ ->
+      case lookup_gen_list t t' l of {
        Prelude.Just s -> Datatypes.Coq_pair (Coq_var s) (Datatypes.Coq_pair l
         m);
        Prelude.Nothing -> Datatypes.Coq_pair (Coq_var m) (Datatypes.Coq_pair
-        (Datatypes.Coq_cons (Datatypes.Coq_pair (Datatypes.Coq_pair (Coq_app
-        f r) x) m) l) (Datatypes.S m))}}}
+        (Datatypes.Coq_cons (Datatypes.Coq_pair (Datatypes.Coq_pair t t') m)
+        l) (Datatypes.S m))}}}
 
 lgen :: Coq_ty -> Coq_ty -> Coq_ty
 lgen t t' =
-  let {
-   filtered_var = Specif.proj1_sig (max_list (Datatypes.app (fv t) (fv t')))}
-  in
-  let {
-   filtered_var0 = Specif.proj1_sig
-                     (lgen_aux t t' Datatypes.Coq_nil filtered_var)}
-  in
-  Datatypes.fst filtered_var0
+  let {s = max_list (Datatypes.app (fv t) (fv t'))} in
+  let {s0 = lgen_aux t t' Datatypes.Coq_nil s} in
+  case s0 of {
+   Datatypes.Coq_pair t1 p -> t1}
+
+lcg :: (Datatypes.Coq_list Coq_ty) -> Prelude.Maybe Coq_ty
+lcg l =
+  case l of {
+   Datatypes.Coq_nil -> Prelude.Nothing;
+   Datatypes.Coq_cons x l' ->
+    case lcg l' of {
+     Prelude.Just s -> Prelude.Just (lgen x s);
+     Prelude.Nothing -> Prelude.Just x}}
+
+tint :: Coq_ty
+tint =
+  Coq_con (Datatypes.S Datatypes.O)
+
+tfloat :: Coq_ty
+tfloat =
+  Coq_con (Datatypes.S (Datatypes.S Datatypes.O))
 
